c648f1014835bd5c431f904cd8d6eb2186524b46
fitrahrestu <fitrahrestu503@gmail.com>
2026-07-28 07:58:43 +0700

    Add files via upload

---
+++ RESTU_KKRPL_BUANA/DASAR_HTML/html_fundamental.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Dasar dasar html
+        </title>
+    </head>
+    <body>
+        <h1 align="center">Pengenalan atribut html</h1>
+        <p align="center">gimana caranya?</p>
+        <p align="center">giinii caranyaa!</p>
+        <h2 align="center">Pengenalan atribut html</h2>
+        <p align="center">gimana caranya?</p>
+        <p align="center">giinii caranyaa!</p>
+        <h3 align="center">Pengenalan atribut html</h3>
+        <p align="center">gimana caranya?</p>
+        <p align="center">giinii caranyaa!</p>
+        <h4 align="center">Pengenalan atribut html</h4>
+        <p align="center">gimana caranya?</p>
+        <p align="center">giinii caranyaa!</p>
+        <h5 align="center">Pengenalan atribut html</h5>
+        <p align="center">gimana caranya?</p>
+        <p align="center">giinii caranyaa!</p>
+        <h6 align="center">Pengenalan atribut html</h6>
+        <p align="center">gimana caranya?</p>
+        <p align="center">giinii caranyaa!</p>
+
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>belajar format format text</title>
+    </head>
+<body>
+    <h1>TEXT</h1>
+    <b>ini format format text tebal</b><br/>
+    <i>ini format text italic</i><br/>
+    <u>ini format text underline</u><br/>
+    <em>ini format text em</em><br/>
+    <small>ini format text small</small><br/>
+    <strong>ini format text strong</strong><br/>
+    <sub>ini format text sub</sub><br/>
+    <sup>ini format text sup</sup><br/>
+    <ins>ini format text ins</ins><br/>
+    <del>ini format text del</del><br/>
+    <mark>ini format text mark</mark>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>belajar membuat paragraf</title>
+    </head>
+<body>
+    <H1>PARAGRAF</H1>
+    <p align="left">1. ini merupakan penulisan paragraf rata kiri
+    dalam dokument HTML</p>
+    <p align="right">2. ini merupakan penulisan paragraf rata
+    kanan dalam dokument HTML</p>
+    <p align="center">3.ini merupakan penulisan paragraf rata
+    tengah dalam dokument HTML</p>
+    <p align="justify">4. ini merupakan penulisan paragraf rata
+    kiri kanan dalam dokument HTML</p>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>belajar membuat tabel di html</title>
+    </head>
+<body>
+    <H1>TABEL</H1>
+    <table border="1">
+    <tr>
+        <td>baris1 / kolom 1</td>
+        <td>baris 1 / kolom 2</td>
+        <td>baris 1 / kolom 3</td>
+    </tr>
+    <tr>
+        <td>baris 2 / kolom 1</td>
+        <td>baris 2 / kolom 2</td>
+        <td>baris 2 / kolom 3</td>
+    </tr>
+    <tr>
+        <td>baris 3/ kolom 1</td>
+        <td>baris 3/ kolom 2</td>
+        <td>baris3/ kolom 3</td>
+    </tr>
+    <tr>
+        <td>baris 4/ kolom 1</td>
+        <td>baris 4/ kolom 2</td>
+        <td>baris 4/ kolom 3</td>
+    </tr>
+    </table>
+</body>
+</html>
+
+ <!DOCTYPE html>
+<html>
+    <head>
+        <title>belajar membuat list</title>
+    </head>
+<body>
+    <h1>MEMBUAT LIST</h1>
+    <ul>
+        <li>belajar HTML</li>
+        <li>belajar PHP</li>
+    </ul>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>DATA</h1>
+
+    <form>
+
+        nama: <input type="text" name="nama"> <br/>
+
+        alamat: <input type="text" name="alamat"> </br/>
+
+        password: <input type="password" name="pass"> <br/>
+
+        Jenis Kelamin:
+
+            <input type="radio" name="jk" value="laki-laki"/> Laki-Laki
+
+            <input type="radio" name="jk" value="perempuan" /> Perempuan
+
+            <br/>
+
+        bahas pemograman yang dikuasai:
+
+            <input type="checkbox" name="web"> Web
+
+            <input type="checkbox" name="mobile"> MOBILE
+
+            <input type="checkbox" name="desk"> DESKTOP
+
+            <br/>
+
+        kota asal:
+
+            <select>
+
+                <option>Semarang</option>
+
+                <option>Bandung</option>
+
+            </select>
+
+            <br/>
+
+        pesan anda:
+
+        <textarea></textarea>
+
+        <br/>
+
+        <input type="submit" value="proses">
+
+    </form>
+
+</body>
+
+</html>
